05a0a71c482c0ea3aaf2afc1c464e6a30b4e66df
Nina Quick <ninaquick@Ninas-MBP.hsd1.or.comcast.net>
2022-02-03 14:17:20 -0800

    add semicolons

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -6,7 +6,7 @@ INTO retirement_titles
 FROM employees e
 JOIN titles as t ON e.emp_no = t.emp_no
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
-ORDER BY e.emp_no
+ORDER BY e.emp_no;
 
 -- show most recent title for active employees of retirement age
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no, rt.first_name, rt.last_name, rt.title
@@ -15,14 +15,14 @@ INTO unique_titles
 LEFT JOIN dept_emp as de
 ON rt.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01')
-ORDER BY rt.emp_no, rt.to_date desc
+ORDER BY rt.emp_no, rt.to_date desc;
 
 -- get a count of soon to retire employees by title
 SELECT COUNT(title) as "employee_count", title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
-ORDER BY "employee_count" desc
+ORDER BY "employee_count" desc;
 
 --- Retrieve current information for employees eligible for mentorship (born in 1965)
 SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
