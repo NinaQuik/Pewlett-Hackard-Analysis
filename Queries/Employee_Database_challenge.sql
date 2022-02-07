32dc3001ef90b19cbc8acfd408459100d52053fb
Nina Quick <ninaquick@Ninas-MBP.hsd1.or.comcast.net>
2022-02-06 17:38:40 -0800

    add query for department stats

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,3 +35,51 @@ ON (e.emp_no = t.emp_no)
 WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 AND de.to_date = ('9999-01-01')
 ORDER BY e.emp_no, t.title;
+
+--See number of soon to retire employees by department
+SELECT  count(e.emp_no) as "employees", d.dept_name
+INTO retirees_by_dept
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE de.to_date = ('9999-01-01')
+AND e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+GROUP BY d.dept_name;
+
+--See number of potential mentors by department
+SELECT  count(e.emp_no) as "employees", d.dept_name
+INTO mentors_by_dept
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE de.to_date = ('9999-01-01')
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+GROUP BY d.dept_name;
+
+--Number of total employees by department
+SELECT  count(e.emp_no) as "employees", d.dept_name
+INTO employees_by_dept
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE de.to_date = ('9999-01-01')
+GROUP BY d.dept_name;
+
+--Breakdown of future retirees and potential mentors per department
+SELECT e.dept_name as "Department", 
+	e.employees as "Total Employees",
+	r.employees as "Retiring Employees", 
+	m.employees as "Mentors", 
+	ROUND((r.employees::decimal / e.employees)* 100, 2) as "Percentage retiring",
+	CEIL(r.employees / m.employees) as "Mentors per open position"
+FROM employees_by_dept as e
+JOIN mentors_by_dept as m 
+ON e.dept_name = m.dept_name
+JOIN retirees_by_dept as r
+ON e.dept_name = r.dept_name;
